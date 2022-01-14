6ac1a3555d9ef109593d14c6e7a6d2c69e39b848
Jacob Nelson <jacob.nlsn@gmail.com>
2022-01-13 18:27:08 -0600

    please work

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ember-quickstart',
     environment,
-    rootURL: '/',
+    rootURL: '/forbiddenbuilds',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
